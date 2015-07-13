53101efc5da6ac093346e589600d59d3847f6f33
Kalinovcic <lovro.kalinovcic@gmail.com>
2015-07-13 18:01:04 +0200

    Setup main and build

---
+++ build/win32/build.bat
@@ -0,0 +1,4 @@
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall"
+cl ..\..\code\cyan.cpp
+
---
+++ code/cyan.cpp
@@ -0,0 +1,13 @@
+/* NOTE(kalinovcic):
+    U ovom fileu je main i sve potrebno da se otvore
+	svi native resursi i pokrene sama igra. */
+
+
+/* NOTE(kalinovcic):
+ 	Pocetak izvrsavanja igre. Otvori window i
+ 	pripremi igru za izvrsavanje. */
+int main(int argc, char** argv) {
+
+
+	return 0;
+}
